b95e49bdfb566111ef21491bc2d8c0a8944c23f4
Yu Wang <yuwang@Yus-MacBook-Air.local>
2017-07-10 16:54:41 -0400

    I try to merge everything to one single json file. But this doesn't work well conceptually. So I try to construct a data frame for further analysis instead.

--- analysis.py
+++ analysis.py
@@ -9,18 +9,18 @@ from collections import defaultdict
 def keyrace(office, folder1, folder2):
     with open(os.path.join(folder1, 'race.json')) as f1:
         ms = json.load(f1)
-        ms['RaceID'] = ""
-
-        lists = ["RaceDetails", "Candidates"]
+        ms['RaceID'] = "99999999"
+        print(ms)
         for file in os.listdir(folder2):
             filename = os.path.basename(file)
-            if ("race_" in filename) & (office in filename) & ( (str(lists[0]) in filename) | (str(lists[1]) in filename)):
+            if ("race_" in filename) & (office in filename) & ( ("RaceDetails" in filename) | ( "Candidates" in filename)):
                 x = re.findall(r'\d+', str(filename))
                 i = x[1]
                 with open(os.path.join(folder2, file)) as f2:
                     son = json.load(f2)
                     son['RaceID'] = i
-
+                    print(son)
+                    raw_input("Enter")
                     dd = defaultdict(list)
                     for d in (ms, son):  # you can list as many input dicts as you want here
                         for key, value in d.iteritems():
@@ -43,8 +43,8 @@ if __name__ == '__main__':
     dir5 = '/Users/yuwang/Documents/research/research/timing/git/mayors/schema'
 
     master = keyrace("Mayor", dir5, dir3)
-    for x in master["RaceID"]:
-        print(x)
+    #for x in master["RaceID"]:
+    #    print(x)
     with open('ddS.json', 'w') as outfile:
         json.dump(master, outfile)
 
